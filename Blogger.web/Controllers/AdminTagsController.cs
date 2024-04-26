2136cfdd6907c818d17f13f0652970fd664cbad6
AyushMahala <ayushmahala2@gmail.com>
2024-04-26 16:13:07 +0530

    First Commit

---
+++ Blogger.web/Controllers/AdminTagsController.cs
@@ -0,0 +1,48 @@
+﻿using Blogger.web.Data;
+using Blogger.web.Models.Domain;
+using Blogger.web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.web.Controllers
+{
+
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+            
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            
+            return RedirectToAction("List");
+        }
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            //use dbcontext to read the text
+            var tags = bloggieDbContext.Tags.ToList();
+            return View(tags);
+        }
+    }
+}
